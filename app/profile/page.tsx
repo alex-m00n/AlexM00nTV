b4b4b4366963fabf1f99f03634ec22727fb6f2d8
AlexM00n <140838530+alex-m00n@users.noreply.github.com>
2024-11-25 21:00:31 +0100

    profile update
    Just a css uptdate

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -10,11 +10,14 @@ export default function Profile() {
         <title>Profile | AlexM00n 🌙</title>
       </head>
       <div className="pt-2 flex justify-center">
-        <div className="align-middle h-96 border-4 items-center w-80 justify-around text-center rounded-lg hover:bg-accent">
+        <div className="profile align-middle h-96 border-4 items-center w-80 justify-around text-center rounded-lg hover:bg-accent">
+          <div className="fixed ml-20 mr-20 items-center bg-background w-40 place-content-center rounded-lg hover:bg-accent" id="img">
+            <br />
+            <img className="w-28 h-auto rounded-full inline-flex" src="/animated.gif" />
+            <h1 className="text-2xl font-bold py-5">AlexM00n</h1>
+          </div>
           <br />
-          <img className="w-28 h-auto rounded-full inline-flex" src="/animated.gif" />
-          <h1 className="text-2xl font-bold py-5">AlexM00n</h1>
-          <hr className="border-2 mx-16 mb-5" />
+          <hr className="border-2 mx-16 mb-5 mt-48" />
           <p>Développeur | Streameur</p>
           <p><i>" Je code ! (づ˶•༝•˶)づ <Heart className="w-4 h-4 inline" /> <Laptop className="w-4 h-4 inline" /> <Heart className="w-4 h-4 inline" /> "</i></p>
           <hr className="border-2 mx-3 my-5" />

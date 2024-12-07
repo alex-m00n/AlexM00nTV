2b0a50f672232fc26d26e3cbde7f7729b00179a7
AlexM00n <140838530+alex-m00n@users.noreply.github.com>
2024-12-07 18:38:51 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import { Button, buttonVariants } from "@/components/ui/button";
 import { HomeIcon } from "@radix-ui/react-icons";
 import clsx from "clsx";
 import { } from "lucide-react";
+import Image from "next/image";
 import Link from "next/link";
 
 export default async function Home() {
@@ -12,7 +13,7 @@ export default async function Home() {
       </head>
 
       <div className="max-w-lg m-4">
-        <img src="/animated.gif" className="rounded-full home-img w-auto"/>
+        <Image src="/animated.gif" className="rounded-full home-img w-auto" alt="profile-picture"/>
         <br />
         <h1 className="max-w-lg font-extrabold text-xl mb-1">Bienvenue sur mon site <Button variant={"link"} >alexm00n.tv</Button></h1>
       </div>

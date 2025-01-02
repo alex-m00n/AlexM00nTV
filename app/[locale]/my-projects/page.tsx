84516a65448822946b48039877829532135e7c0e
AlexM00n <140838530+alex-m00n@users.noreply.github.com>
2025-01-02 19:49:23 +0100

    projects update

--- app/[locale]/my-projects/page.tsx
+++ app/[locale]/my-projects/page.tsx
@@ -40,7 +40,7 @@ export default async function Projects() {
                                 <div className="rounded-lg border hover:bg-accent w-96">
                                     <div className="rounded-full bg-red-600 w-20 h-20 -mt-10 ml-5"><center><Bot className="align-middle" size={75} /></center></div>
                                     <div className="p-1">
-                                        <h1 className="m-2">alexm00n.tv</h1>
+                                        <h1 className="m-2">M00n Bot</h1>
                                         <p><Tag className="inline" /> Discord.js</p>
                                         <p className="mx-2">
                                             {t("description2")}
@@ -77,7 +77,7 @@ export default async function Projects() {
                         <div className="rounded-lg border hover:bg-accent w-96">
                             <div className="rounded-full bg-red-600 w-20 h-20 -mt-10 ml-5"><center><Bot className="align-middle" size={75} /></center></div>
                             <div className="p-1">
-                                <h1 className="m-2">alexm00n.tv</h1>
+                                <h1 className="m-2">M00n Bot</h1>
                                 <p><Tag className="inline" /> Discord.js</p>
                                 <p className="mx-2">
                                     {t("description2")}

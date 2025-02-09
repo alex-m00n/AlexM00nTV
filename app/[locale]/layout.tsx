3d8624db679a4f1bbabfe0788909ebca12524b56
AlexM00n <140838530+alex-m00n@users.noreply.github.com>
2025-02-09 16:25:56 +0100

    Vercel SpeedInsights Update

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -6,6 +6,7 @@ import clsx from "clsx";
 import { Header } from "@/src/features/layout/Header";
 import { Footer } from "@/src/features/layout/Footer";
 import { Providers } from "./providers";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const ibm = IBM_Plex_Mono({ weight: "500", subsets: ["latin"] });
 
@@ -33,6 +34,7 @@ export default function RootLayout({
             <Header/>
             <div className="flex-1 m-auto pt-16">
               {children}
+              <SpeedInsights />
             </div>
           </div>
 
